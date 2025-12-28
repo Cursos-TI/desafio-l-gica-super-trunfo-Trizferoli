46265cace6282832797fb2528fff1869272c5c05
trizferoli <beatriz.oliveira@zig.fun>
2025-12-28 13:24:20 -0300

    feat: add code

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -8,36 +8,140 @@
 int main() {
     // Definição das variáveis para armazenar as propriedades das cidades
     // Você pode utilizar o código do primeiro desafio
+    char state1;
+    char cardCode1[3];
+    char cityName1[100];
+    int population1;
+    float area1;
+    float PIB1;
+    int touristSitesCount1;
 
     
     // Cadastro das Cartas:
     // Implemente a lógica para solicitar ao usuário que insira os dados das cidades
-    // utilizando a função scanf para capturar as entradas.
-    // utilize o código do primeiro desafio
 
-    // Exemplo:
-    // printf("Digite o código da cidade: ");
-    // scanf("%s", codigo);
-    // 
-    // (Repita para cada propriedade)
+    printf("Digite o código da cidade: ");
 
-    // Comparação de Cartas:
-    // Desenvolva a lógica de comparação entre duas cartas.
-    // Utilize estruturas de decisão como if, if-else para comparar atributos como população, área, PIB, etc.
+    scanf("%c", &state1);
 
-    // Exemplo:
-    // if (populacaoA > populacaoB) {
-    //     printf("Cidade 1 tem maior população.\n");
-    // } else {
-    //     printf("Cidade 2 tem maior população.\n");
-    // }
+    printf("Digite o código da carta: ");
+    scanf("%s", cardCode1);
 
-    // Exibição dos Resultados:
-    // Após realizar as comparações, exiba os resultados para o usuário.
-    // Certifique-se de que o sistema mostre claramente qual carta venceu e com base em qual atributo.
+    printf("Digite o nome da cidade: ");
+    scanf("%s", cityName1);
 
-    // Exemplo:
-    // printf("A cidade vencedora é: %s\n", cidadeVencedora);
+    printf("Digite a população da cidade: ");
+    scanf("%d", &population1);
+
+    printf("Digite a área da cidade: ");
+    scanf("%f", &area1);
+
+    printf("Digite o PIB da cidade: ");
+    scanf("%f", &PIB1);
+
+    printf("Digite o número de pontos turísticos da cidade: ");
+    scanf("%d", &touristSitesCount1);
+
+
+    char state2;
+    char cardCode2[3];
+    char cityName2[100];
+    int population2;
+    float area2;
+    float PIB2;
+    int touristSitesCount2;
+
+    printf("AGORA INSIRA AS INFORMAÇÕES DA SEGUNDA CIDADE: \n");
+
+    printf("Digite o código da cidade: ");
+    scanf("%c", &state2);
+
+    printf("Digite o código da carta: ");
+    scanf("%s", cardCode2);
+
+    printf("Digite o nome da cidade: ");
+    scanf("%s", cityName2);
+
+    printf("Digite a população da cidade: ");
+    scanf("%d", &population2);
+
+    printf("Digite a área da cidade: ");
+    scanf("%f", &area2);
+
+    printf("Digite o PIB da cidade: ");
+    scanf("%f", &PIB2);
+
+    printf("Digite o número de pontos turísticos da cidade: ");
+    scanf("%d", &touristSitesCount2);
+    
+
+    char *winsOfCity1[4];
+    int winsOfCity1Index = 0;
+
+    char *winsOfCity2[4];
+    int winsOfCity2Index = 0;
+
+    char *draws[4];
+    int drawsIndex = 0;
+
+    if (population1 > population2) {
+        winsOfCity1[winsOfCity1Index] = "população";
+        winsOfCity1Index++;
+    } else if (population1 < population2) {
+        winsOfCity2[winsOfCity2Index] = "população";    
+        winsOfCity2Index++;
+    } else {
+        draws[drawsIndex] = "população" ;
+        drawsIndex++;
+    }
+
+
+    if (area1 > area2) {
+        winsOfCity1[winsOfCity1Index] = "área";
+        winsOfCity1Index++;
+    } else if (area1 < area2) {
+        winsOfCity2[winsOfCity2Index] = "área";
+        winsOfCity2Index++;
+    } else {
+        draws[drawsIndex] = "área";
+        drawsIndex++;
+    }
+
+    if (PIB1 > PIB2) {
+        winsOfCity1[winsOfCity1Index] = "PIB";
+        winsOfCity1Index++;
+    } else if (PIB1 < PIB2) {
+        winsOfCity2[winsOfCity2Index] = "PIB";
+        winsOfCity2Index++;
+    } else {
+        draws[drawsIndex] = "PIB";
+        drawsIndex++;
+    }
+
+    if (touristSitesCount1 > touristSitesCount2) {
+        winsOfCity1[winsOfCity1Index] = "pontos turísticos";
+        winsOfCity1Index++;
+    } else if (touristSitesCount1 < touristSitesCount2) {
+        winsOfCity2[winsOfCity2Index] = "pontos turísticos";
+        winsOfCity2Index++;
+    } else {
+        draws[drawsIndex] = "pontos turísticos";
+        drawsIndex++;}
+
+    if (winsOfCity1Index > winsOfCity2Index) {
+        printf("A cidade %s venceu a comparação com %d vitórias e %d empates. \n", cityName1, winsOfCity1Index, drawsIndex);
+        for (int i = 0; i < winsOfCity1Index; i++) {
+            printf("Categoria: %s\n", winsOfCity1[i]);
+        }
+    } else if (winsOfCity1Index < winsOfCity2Index) {
+        printf("A cidade %s venceu a comparação com %d vitórias e %d empates. \n", cityName2, winsOfCity2Index, drawsIndex);
+        for (int i = 0; i < winsOfCity2Index; i++) {
+            printf("Categoria: %s\n", winsOfCity2[i]);
+        }
+    } 
+    else {
+        printf("As duas cidades empataram");
+    }
 
     return 0;
 }
